928d702add6e57d258faa42a036fd920c94a54c2
Kriti Nandakumar <nandakuk@purdue.edu>
2025-09-23 15:27:24 -0400

    Created the UI for the home page of the app

--- DIET/app/index.tsx
+++ DIET/app/index.tsx
@@ -1,20 +1,54 @@
-import { Link } from 'expo-router';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function HomeScreen() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: 'Earhart', value: '1' },
+    { label: 'Ford', value: '2' },
+    { label: 'Hillenbrand', value: '3' },
+    { label: 'Wiley', value: '4' },
+    { label: 'Windsor', value: '5' },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <Link href="/details">View details</Link>
+      <Text style={styles.titleText}>Boiler DIET</Text>
+      <Text style={styles.descText}>(Dining Image & Evaluation Tracker)</Text>
+      <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      style={styles.hallPicker}
+    />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#CEB888',
   },
+  titleText: {
+    marginTop: 100,
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  descText:{
+    marginBottom: 150,
+  },
+  hallPicker:{
+    marginBottom: 500,
+    marginRight: 50,
+    marginLeft: 50,
+    width: 300,
+  }
 });
   
