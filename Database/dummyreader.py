66615d5a2b09eb70208c3fe519aec6673b4e6cc9
Arnav Singh <sing2415@purdue.edu>
2025-09-30 01:20:22 -0400

    Made comprehensive database, updated user code function

--- Database/dummyreader.py
+++ Database/dummyreader.py
@@ -7,11 +7,6 @@ from datetime import datetime, timedelta
 def random_id(length=8):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
-def random_timestamp(start, end):
-    delta = end - start
-    seconds = random.randint(0, int(delta.total_seconds()))
-    return start + timedelta(seconds=seconds)
-
 load_dotenv()
 db_password = os.getenv("DB_PASSWORD")
 
@@ -28,41 +23,30 @@ print("Column Info:")
 for col in columns:
     print(col)
 
-dininghalls = ["Earhart","Ford","Hillenbrand","Wiley","Windsor"]
-types =["breakfast","lunch","dinner"]
-
 cursor.execute("SELECT * FROM users LIMIT 1")
 b = cursor.fetchall()
 if not b:
     print("Adding dummy data.")
     dummy_data = []
-    start = datetime(2023, 1, 1)
-    end = datetime(2025, 1, 1)
     for i in range(20):
         uid = random_id()
         fname = f"fname{i}"
         lname = f"lname{i}"
         email = f"user{i}@purdue.edu"
-        ts = random_timestamp(start, end).strftime('%Y-%m-%d %H:%M:%S')
-        typeMeal = random.choice(types)
-        loc = random.choice(dininghalls)
-        dummy_data.append((uid, fname, lname, email, ts, typeMeal, loc))
+        username = f"user{i}"
+        dummy_data.append((uid, fname, lname, email, username))
     sql = """
-    INSERT INTO users (user_id, fname, lname, email, created_at, type, location)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO users (UserId, FirstName, LastName, Email, Username)
+    VALUES (%s, %s, %s, %s, %s)
     """
     cursor.executemany(sql, dummy_data)
     cnx.commit()
 
-
-cursor.execute("SELECT fname, lname, email, type, location, created_at FROM users")
+cursor.execute("SELECT FirstName, LastName, Email, Username FROM users")
 all_users = cursor.fetchall()
 for user in all_users:
-    fname, lname, email, meal_type, location, created_at = user
-    print(f"{fname} {lname} with email {email} ate {meal_type} at {location} on {created_at}")
-
-
-
+    fname, lname, email, username = user
+    print(f"{fname} {lname} with email {email} has username {username}")
 
 cursor.close()
 cnx.close()
