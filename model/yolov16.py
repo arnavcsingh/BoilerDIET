48322ffe111456e0f35e8e32163d0c45b3e0c65b
Arnav Singh <sing2415@purdue.edu>
2026-03-03 17:55:55 -0500

    Adding lower res models

---
+++ model/yolov16.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")
+
+model.train(
+    data="vfn_yolo/data.yaml",
+    epochs=100,
+    imgsz=416,
+    batch=16,
+    project="runs_vfn",
+    name="yolov8n_416"
+)
